ac859f3185f3483dd6ff0c024e2db6338e29e809
Rodrigo Porto <vgnporto@gmail.com>
2022-07-06 15:23:43 -0300

    Setup slick settings

--- settings.js
+++ settings.js
@@ -3,4 +3,19 @@ $(".bannerSlider").slick({
   infinite: false,
   slidesToShow: 3,
   arrows: true,
+  mobileFirst: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
